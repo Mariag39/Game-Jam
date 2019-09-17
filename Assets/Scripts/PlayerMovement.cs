c57d01060ea7bb6c036f32ccc5a9c933c126f26d
Celia Castaños <celica02@ucm.es>
2019-09-17 12:24:27 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    //Para que mire a la izqd -> Y = 90
+    //                   der  -> Y = -90
+    //                   abj  -> Y = 0
+    //                   arr  -> Y = 180
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+        {
+            //transform.Rotate(Vector3.down, 90f);
+            gameObject.transform.LookAt(Vector3.down);
+            //gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, posFin, 0.3f);
+        }
+    }
+}
