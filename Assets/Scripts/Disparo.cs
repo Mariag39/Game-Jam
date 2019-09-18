d867a132dd046f888ae7d43046fc6e4da888b235
Filinouns <andpuent@ucm.es>
2019-09-18 18:15:58 +0200

    Disparo y movement

---
+++ Assets/Scripts/Disparo.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;              // Used to identify the different players.
+    public GameObject m_Shell;                   // Prefab of the shell.
+    public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
+    public float m_LaunchForce = 15f;
+    public float fireRate = 1f;
+
+    private string m_FireButton;                // The input axis that is used for launching shells.
+    private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
+    private float nextFireTime;
+
+    private void OnEnable() {}
+
+    private void Start() {
+        // The fire axis is based on the player number.
+        m_FireButton = "Fire" + m_PlayerNumber;
+    }
+
+    private void Update() {
+        if (!m_Fired) {
+            Fire(m_LaunchForce, fireRate);
+        }
+        // Otherwise, if the fire button has just started being pressed...
+        if (Input.GetButtonDown(m_FireButton)) {
+            m_Fired = false;
+        }
+        // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
+        else if (Input.GetButton(m_FireButton) && !m_Fired) {
+            Fire(m_LaunchForce, fireRate);
+        }
+        // Otherwise, if the fire button is released and the shell hasn't been launched yet...
+        else if (Input.GetButtonUp(m_FireButton) && !m_Fired) {
+            Fire(m_LaunchForce, fireRate);
+
+        }
+    }
+
+    public void Fire(float launchForce, float fireRate) {
+        if (Time.time > nextFireTime) {
+            nextFireTime = Time.time + fireRate;
+            // Set the fired flag so only Fire is only called once.
+            m_Fired = true;
+
+            // Create an instance of the shell and store a reference to it's rigidbody.
+            GameObject shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as GameObject;
+
+            // Set the shell's velocity to the launch force in the fire position's forward direction.
+            shellInstance.GetComponent<Rigidbody>().velocity = m_LaunchForce * m_FireTransform.forward;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,52 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
+    public float pSpeed;
 
-    float pSpeed;
+    private bool l;
+    private bool r;
+    private bool u;
+    private bool d;
 
     // Use this for initialization
-    void Start()
-    {
+    void Start() {
         pSpeed = 0.1f;
     }
 
-    void Update()
-    {
+    void Update() {
+        movementInfo();
+    }
 
+    void movementInfo() {
         if (Input.GetKey(KeyCode.LeftArrow))
-        {
-
-            LookAt(-1, 0);
-            Move(-1, 0 );
+        {  //Izquierda
+            Move(-1, 0);
+            l = true;
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else l = false;
+        if (Input.GetKey(KeyCode.RightArrow))   //Derecha
         {
             LookAt(1, 0);
             Move(1, 0);
-
+            r = true;
         }
-        else if (Input.GetKey(KeyCode.UpArrow))
+        else r = false;
+        if (Input.GetKey(KeyCode.UpArrow))  //Arriba
         {
-            LookAt(0, 1);
+
             Move(0, 1);
+            u = true;
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
+        else u = false;
+        if (Input.GetKey(KeyCode.DownArrow))    //Abajo
         {
             LookAt(0, -1);
             Move(0, -1);
+            d = true;
         }
+        else d = false;
+
+        // Horientacion
+        if (l && u) LookAt(-1, 1);
+        else if (l && d) LookAt(-1, -1);
+        else if (r && u) LookAt(1, 1);
+        else if (r && d) LookAt(1, -1);
+        else if (u) LookAt(0, 1);
+        else if (d) LookAt(0, -1);
+        else if (l) LookAt(-1, 0);
+        else if (r) LookAt(1, 0);
     }
 
-    void LookAt(int x, int z)
-    {
+    void LookAt(int x, int z) {
         Vector3 pPlayer = this.transform.position;
         gameObject.transform.LookAt(new Vector3(pPlayer.x + x, pPlayer.y, pPlayer.z + z));
 
     }
-    void Move(int x, int z)
-    {
+
+    void Move(int x, int z) {
         Vector3 pPlayer = this.transform.position;
         if (x != 0)
             pPlayer.x += pSpeed * x;
