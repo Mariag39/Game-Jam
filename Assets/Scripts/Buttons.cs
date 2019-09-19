8b271618430d007790191e6586e6775ddbb4aa58
Celia Castaños <celica02@ucm.es>
2019-09-19 12:22:35 +0200

    Menú empezado

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Buttons : MonoBehaviour {
+
+    public void GoToGameScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void GoToControlsScene()
+    {
+        SceneManager.LoadScene("ControlsScene");
+    }
+    public void GoToCreditsScene()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+}
